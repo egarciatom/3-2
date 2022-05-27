808db6a9f6e6949b2cde69411a233404705bcff8
egarciatom <101406426+egarciatom@users.noreply.github.com>
2022-05-27 13:45:53 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+let sprite: game.LedSprite = null
+input.onButtonPressed(Button.A, function () {
+    sprite.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        sprite.set(LedSpriteProperty.X, 0)
+        basic.pause(500)
+    }
+})
+basic.forever(function () {
+	
+})
