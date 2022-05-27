41ebb6869db022f7c8490ffb0e305e93fce8679b
egarciatom <101406426+egarciatom@users.noreply.github.com>
2022-05-27 13:52:40 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
-let sprite: game.LedSprite = null
 input.onButtonPressed(Button.A, function () {
-    sprite.move(-1)
+    puntua.move(-1)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.AB, function () {
     for (let index = 0; index < 4; index++) {
+        let sprite: game.LedSprite = null
         sprite.set(LedSpriteProperty.X, 0)
         basic.pause(500)
     }
+    basic.showIcon(IconNames.Chessboard)
+    basic.showIcon(IconNames.SmallDiamond)
+    for (let index = 0; index < 4; index++) {
+        puntua.set(LedSpriteProperty.Y, -4)
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    puntua.move(1)
+})
+let puntua: game.LedSprite = null
+puntua = game.createSprite(2, 0)
 basic.forever(function () {
 	
 })
